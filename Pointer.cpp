73279c6e61bf9c73a81bce18ff821bb196ea863f
Yuvrajchandra <53931942+Yuvrajchandra@users.noreply.github.com>
2020-05-27 20:27:02 +0530

    Create Pointer.cpp

---
+++ Pointer.cpp
@@ -0,0 +1,23 @@
+#include <cstdlib>
+#include <stdio.h>
+#include<math.h>
+
+void update(int *a,int *b) 
+{
+    int c= *a + *b;
+    int d= abs((*a) - (*b));
+    *a=c;
+    *b=d;
+
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
